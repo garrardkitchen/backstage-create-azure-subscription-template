4c08607632955825225e995e46b477d5483430f4
Garrard Kitchen <garrardkitchen@gmail.com>
2024-09-15 17:07:55 +0100

    refactor: Added new vars

--- variables.tf
+++ variables.tf
@@ -13,32 +13,42 @@ variable "subscription_name" {
   default = "Spoke MCK Uvance Platform Engineering"
 }
 
-variable "subscription_tags" {
-  type = map(string)
-  default = {
-    Department = "Platform Engineering"
-    CostCenter = "0000001"
-    Project    = "Uvance"
-    CreatedBy  = "garrard.kitchen@fujitsu.com"
-  }
+variable "resource_group_name" {
+  type    = string
+  detault = "${{ values.rgName }}"
 }
 
-variable "lz_type" {
-  type        = string
-  description = "Used in tags to show the LZ type is either 'platform' or 'application'."
-  default     = "none"
-  validation {
-    condition     = can(regex("^(platform|application)$", lower(var.lz_type)))
-    error_message = "Invalid deployment_type. Allowed values are 'platform', or 'application'."
-  }
+variable "resource_group_location" {
+  type    = string
+  detault = "${{ values.rgLocation }}"
 }
 
-variable "lz_platform_type" {
-  type        = string
-  description = "Used in tags when lz_type is 'platform'."
-  default     = ""
-  validation {
-    condition     = (lower(var.lz_type) == "platform" && can(regex("^(connectivity|identity|management)$", lower(var.lz_platform_type))) || lower(var.lz_type) == "application" && lower(var.lz_platform_type) == "")
-    error_message = "Invalid platform_module value. Allowed values if LZ_TYPE is Platform are 'connectivity', 'identity', or 'management'.  It should be empty if LZ_TYPE is Application."
-  }
-}
+# variable "subscription_tags" {
+#   type = map(string)
+#   default = {
+#     Department = "Platform Engineering"
+#     CostCenter = "0000001"
+#     Project    = "Uvance"
+#     CreatedBy  = "garrard.kitchen@fujitsu.com"
+#   }
+# }
+
+# variable "lz_type" {
+#   type        = string
+#   description = "Used in tags to show the LZ type is either 'platform' or 'application'."
+#   default     = "none"
+#   validation {
+#     condition     = can(regex("^(platform|application)$", lower(var.lz_type)))
+#     error_message = "Invalid deployment_type. Allowed values are 'platform', or 'application'."
+#   }
+# }
+
+# variable "lz_platform_type" {
+#   type        = string
+#   description = "Used in tags when lz_type is 'platform'."
+#   default     = ""
+#   validation {
+#     condition     = (lower(var.lz_type) == "platform" && can(regex("^(connectivity|identity|management)$", lower(var.lz_platform_type))) || lower(var.lz_type) == "application" && lower(var.lz_platform_type) == "")
+#     error_message = "Invalid platform_module value. Allowed values if LZ_TYPE is Platform are 'connectivity', 'identity', or 'management'.  It should be empty if LZ_TYPE is Application."
+#   }
+# }
